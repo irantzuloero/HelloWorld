2a79947abaeac973ea8f57b403edabf3f9ff1089
USUARIO <USUARIO@192.168.1.134>
2024-03-09 11:03:10 +0100

    Message klasea gehituta + HelloWorld aldatuta

--- HelloWorld/src/gui/HelloWorld.java
+++ HelloWorld/src/gui/HelloWorld.java
@@ -1,42 +1,55 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import domain.Message;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.util.HashMap;
+
 public class HelloWorld extends JFrame {
-private JPanel contentPane;
-/**
-* Launch the application.
-*/
-public static void main(String[] args) {
-EventQueue.invokeLater(new Runnable() {
-public void run() {
-try {
-HelloWorld frame = new HelloWorld();
-frame.setVisible(true);
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
-});
-}
-/**
-* Create the frame.
-*/
-public HelloWorld() {
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setBounds(100, 100, 450, 300);
-contentPane = new JPanel();
-contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-setContentPane(contentPane);
-contentPane.setLayout(null);
-JLabel lblHelloWorld = new JLabel("Kaixo Mundua");
-lblHelloWorld.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
-lblHelloWorld.setBounds(115, 93, 225, 44);
-contentPane.add(lblHelloWorld);
-}
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HashMap<String, String> hm = new HashMap<String, String>();
+					hm.put("eu", "Kaixo Mundua");
+					hm.put("es", "Hola Mundo");
+					Message m = new Message("hello world", hm);
+					HelloWorld frame = new HelloWorld(m, "eu");
+					
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelloWorld(Message m, String lang){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHelloWorld = new JLabel(m.getTraductions(lang));
+		lblHelloWorld.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblHelloWorld.setBounds(115, 93, 225, 44);
+		contentPane.add(lblHelloWorld);
+	}
 }
